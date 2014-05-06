3f5f9034d742b8b1ba449a111ff80d44b6206674
rajincse <rajin.cse@gmail.com>
2014-05-06 01:26:26 -0400

    #8
    All tables are changed to MyISAM engine and default charset latin1

--- MySQLScripts/imdb_small.sql
+++ MySQLScripts/imdb_small.sql
@@ -11,8 +11,9 @@ CREATE TABLE movie
 	id INT PRIMARY KEY,
 	title TEXT,
 	production_year INT,
-	rating DECIMAL(10,2)
-);
+	rating DECIMAL(10,2),
+	KEY idx_title (title(10))
+)ENGINE = MyISAM DEFAULT CHARSET=latin1;
 
 INSERT INTO movie ( id, title, production_year, rating)
 SELECT 
@@ -25,17 +26,16 @@ FROM imdb.title AS T
 LEFT OUTER JOIN imdb.movie_info_idx AS MI ON MI.movie_id = T.id AND MI.info_type_id=101
 WHERE
 T.kind_id = 1;
-
-CREATE INDEX idx_title ON movie(title(242));
+ 
 
 -- Genre Table
 
 CREATE TABLE genre
 (
 	id INT AUTO_INCREMENT PRIMARY KEY, 
-	genre TEXT
-);
-CREATE INDEX idx_genre ON genre(genre(44));
+	genre TEXT,
+	KEY idx_genre (genre(10))
+)ENGINE = MyISAM DEFAULT CHARSET=latin1;
 
 INSERT INTO genre ( genre)
 SELECT
@@ -49,7 +49,8 @@ CREATE TABLE movie_genre
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	movie_id INT,
 	genre_id INT
-);
+)ENGINE = MyISAM DEFAULT CHARSET=latin1;
+
 INSERT INTO movie_genre( movie_id, genre_id)
 SELECT 
 	T.id, 
@@ -64,28 +65,26 @@ INNER JOIN imdb_small.genre AS G ON G.genre = COALESCE(MI.info,'');
 CREATE TABLE person
 (
 	id INT PRIMARY KEY,
-	first_name TEXT,
-	last_name TEXT,
-	gender VARCHAR(1)
-);
-CREATE INDEX idx_first_name ON person(first_name(106));
-CREATE INDEX idx_last_name ON person(last_name(106));
+	`name` TEXT,
+	gender VARCHAR(1),
+	KEY idx_name (`name`(6))
+) ENGINE = MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO  person(id, first_name, last_name, gender)
+
+
+INSERT INTO  person(id, `name`, gender)
 SELECT   
 	N.id,  
-	SUBSTRING(N.`name`,INSTR(N.`name`,',')+1, LENGTH(N.`name`)) AS first_name, 
-	SUBSTRING(N.`name`,1, INSTR(N.`name`,',')-1) AS last_name, 
+	CONCAT
+	(
+		SUBSTRING(N.`name`,INSTR(N.`name`,',')+1, LENGTH(N.`name`))
+		, ' '
+		,SUBSTRING(N.`name`,1, INSTR(N.`name`,',')-1)
+	) AS `name`, 
 	 gender
  FROM imdb.`name` AS N 
 WHERE   
 N.gender IS NOT NULL;
-
-ALTER TABLE person ADD COLUMN `name` TEXT;
-CREATE INDEX idx_name ON person(`name`(106));
-
-UPDATE person 
-SET `name` = CONCAT(first_name, ' ', last_name);
 
 -- cast info
 -- mysqldump -u root -prajin imdb cast_info > C:\Users\rajin\Documents\dumps\cast_info.sql
@@ -98,9 +97,9 @@ CREATE TABLE biography
 (
 	id INT PRIMARY KEY,
 	person_id INT, 
-	biography TEXT
-);
-CREATE INDEX idx_bio_person_id ON biography(person_id);
+	biography TEXT,
+	KEY idx_bio_person_id (person_id)
+)ENGINE = MyISAM DEFAULT CHARSET=latin1;
 
 INSERT INTO biography ( id, person_id, biography)
 SELECT 

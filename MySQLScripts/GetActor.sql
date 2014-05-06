f853a4cb8b434afddbea652caceada7ceccc70ab
rajincse <rajin.cse@gmail.com>
2014-05-06 01:45:24 -0400

    #8
    imdb_small compatible queries added

--- MySQLScripts/GetActor.sql
+++ MySQLScripts/GetActor.sql
@@ -1,5 +1,11 @@
 USE IMDB;
--- 437747 DiCaprio, Leonardo
+-- 437747 Leonardo DiCaprio
+-- 700661 Tom Hanks
+-- 369071 Tom Cruise
+-- 1304615 Al Pacino
+-- 95992 Christian Bale
+
+
 SELECT   
 	N.id,  
 	N.`name`,  
@@ -7,7 +13,7 @@ SELECT
  FROM `name` AS N                        
 WHERE   
 N.gender IS NOT NULL AND   
-N.`name` LIKE '%Dicaprio, Leonardo%' 
+N.`name` LIKE '%Bale, Christian%' 
 LIMIT 0,10 ;
 
 SELECT 	 
@@ -16,15 +22,44 @@ SELECT
 		C.role_id, 
 		T.id, 
 		T.title AS title,  
-		COALESCE(T.production_year,0) AS `year` , 
-		COALESCE(PI.info,'') AS biography 
+		COALESCE(T.production_year,0) AS `year` 
+	 	,COALESCE(PI.info,'') AS biography 
 FROM  
 title AS T 
 LEFT OUTER JOIN movie_info_idx AS MI ON MI.movie_id = T.id AND MI.info_type_id=101 
 INNER JOIN cast_info AS C ON C.movie_id = T.id AND (C.role_id = 1 OR C.role_id=2 OR C.role_id=8) AND T.kind_id=1 
 INNER JOIN `name` AS N ON N.id = C.person_id 
- LEFT OUTER JOIN person_info AS PI ON N.id = PI.person_id AND PI.info_type_id=19 
+LEFT OUTER JOIN person_info AS PI ON N.id = PI.person_id AND PI.info_type_id=19 
 WHERE  
-N.id = 437747 
+N.id = 95992 
 ORDER BY COALESCE(MI.info,0) DESC ;
+
+-- Imdb small
+USE imdb_small;
+
+SELECT   
+	N.id,  
+	`name`,  
+	N.gender 
+ FROM person AS N                        
+WHERE   
+`name` LIKE '%Christian Bale%' 
+LIMIT 0,10 ;
+
+SELECT 	 
+		N.`name`,
+		N.gender, 
+		C.role_id, 
+		T.id, 
+		T.title AS title,  
+		T.production_year AS `year` 
+		 ,B.biography AS biography 
+FROM  
+movie AS T 
+INNER JOIN cast_info AS C ON C.movie_id = T.id  AND (C.role_id = 1 OR C.role_id=2 OR C.role_id=8) 
+INNER JOIN person AS N ON N.id = C.person_id 
+  INNER JOIN biography AS B ON B.person_id = N.id
+WHERE  
+N.id = 95992 
+ORDER BY T.rating DESC ;
 
--- MySQLScripts/GetMovie.sql
+++ MySQLScripts/GetMovie.sql
@@ -1,5 +1,9 @@
 USE imdb;
 
+-- 2236590 Inception
+-- 2144576 Forrest Gump
+-- 2359532 Magnolia
+-- 2666902 The GodFather
 -- 2652712 The Dark Knight
 -- Search 
 SELECT 	T.id, 
@@ -7,7 +11,7 @@ SELECT 	T.id,
 		COALESCE(T.production_year,0) AS `year` 
 FROM title AS T 
 WHERE   
- T.title LIKE '%Dark Knight%' 
+ T.title LIKE '%The Dark Knight%' 
 AND T.kind_id = 1 
 LIMIT 0,10 ;
 
@@ -30,4 +34,32 @@ INNER JOIN `name` AS N ON N.id = C.person_id
 WHERE  
 T.id =  2652712
 ORDER BY COALESCE(C.nr_order,1000);
+
+-- Imdb small
+USE imdb_small;
+
+SELECT 	T.id, 
+		T.title,  
+		T.production_year 
+FROM movie AS T 
+WHERE   
+ T.title LIKE '%The Dark Knight%' 
+LIMIT 0,10 ;
+
+SELECT 	T.title AS title,  
+		T.production_year,   
+		T.rating, 
+		C.role_id, 
+		N.id,  
+		N.`name`, 
+		N.gender 
+		 
+FROM  
+movie AS T 
+INNER JOIN cast_info AS C ON C.movie_id = T.id 
+ AND ( C.role_id=8 OR C.role_id = 1 OR C.role_id =2) 
+INNER JOIN person AS N ON N.id = C.person_id 
+WHERE  
+T.id =  2652712
+ORDER BY C.nr_order;
 

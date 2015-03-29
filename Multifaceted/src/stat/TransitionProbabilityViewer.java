57e66bcb1658a82ba084aa50f4cd45387ceed4cf
rajincse <rajin.cse@gmail.com>
2015-03-29 02:52:12 -0400

    #28
    1. Possibility count is incorporated
    2. Relevance data reading option added

--- Multifaceted/src/stat/TransitionProbabilityViewer.java
+++ Multifaceted/src/stat/TransitionProbabilityViewer.java
@@ -10,7 +10,12 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import eyetrack.EyeTrackerItem;
 
@@ -24,23 +29,56 @@ import perspectives.two_d.JavaAwtRenderer;
 public class TransitionProbabilityViewer extends Viewer implements JavaAwtRenderer {
 
 	public static final String PROPERTY_LOAD = "Load";
+	public static final String PROPERTY_LOAD_RELEVANCE_DATA = "Load Relevance";
 	
-	public static final int OFFSET_NOT_CONNECTED =0;
-	public static final int OFFSET_CONNECTED =1;
-	public static final int OFFSET_CONNECTED_HOVER =2;
-	public static final int TOTAL_OFFSETS =3;
+	public static final String TAG_HOVER="HOV";
+	public static final String TAG_VIEW ="VIEW";
+	
+	public static final int TOTAL_TYPES =6;
+	public static final int TOTAL_CONNECTION_TYPE=6;
+	
+	public static final int CONNECTION_NOT_CONNECTED_NO_HOVER =0;
+	public static final int CONNECTION_NOT_CONNECTED_SOURCE_HOVER =1;
+	public static final int CONNECTION_NOT_CONNECTED_DESTINATION_HOVER =2;
+	public static final int CONNECTION_CONNECTED_NO_HOVER =3;
+	public static final int CONNECTION_CONNECTED_SOURCE_HOVER =4;
+	public static final int CONNECTION_CONNECTED_DESTINATION_HOVER =5;
+	
+	
 	
 	public static final String[] TYPE_NAME =new String[]{"INIT", "ACTOR", "MOVIE", "DIRECTOR", "GENRE", "STAR"};
-	public static final String[] OFFSET_NAME =new String[]{"NOT CONNECTED", "CONNECTED", "HOVER"};
+	public static final String[] CONNECTION_NAME =new String[]{"NOT CONNECTED NO HOVER",
+														   "NOT CONNECTED SOURCE HOVER",
+														   "NOT CONNECTED DESTINATION HOVER",
+														   "CONNECTED NO HOVER ",
+														   "CONNECTED SOURCE HOVER",
+														   "CONNECTED DESTINATION HOVER"};
 	
 	public static final int COLUMNS_PER_ELEMENT =4;
 	
-	private int [][] countMatrix = new int[6][6*TOTAL_OFFSETS];
+	private int [][][] countMatrix = new int[TOTAL_TYPES][TOTAL_TYPES][TOTAL_CONNECTION_TYPE];
+	private int [][][] possibilityMatrix = new int[TOTAL_TYPES][TOTAL_TYPES][TOTAL_CONNECTION_TYPE];
+	
+	private ArrayList<StatElement> elementNames = new ArrayList<StatElement>();
 	private IMDBDataSource data = null;
 	
+	private int currentTask=0;
+	private StatElement currentView =null;
 	public TransitionProbabilityViewer(String name, IMDBDataSource data) {
 		super(name);
 		this.data = data;
+		
+		Property<PFileInput> pLoadRelevance = new Property<PFileInput>(PROPERTY_LOAD_RELEVANCE_DATA, new PFileInput())
+				{
+					@Override
+					protected boolean updating(PFileInput newvalue) {
+						// TODO Auto-generated method stub
+						loadSavedData(newvalue.path);
+						return super.updating(newvalue);
+					}
+			
+				};
+		addProperty(pLoadRelevance);
 		
 		PFileInput directoryOnly = new PFileInput();
 		directoryOnly.onlyDirectories = true;
@@ -57,7 +95,31 @@ public class TransitionProbabilityViewer extends Viewer implements JavaAwtRender
 				};
 		addProperty(pLoad);
 	}
-	
+	private void loadSavedData(String filePath)
+	{
+		try {
+			File file =new File(filePath);
+			FileInputStream fOutput = new FileInputStream(file);
+		
+			ObjectInputStream out = new ObjectInputStream(fOutput);
+			
+			RelevanceData data = (RelevanceData) out.readObject();
+			this.elementNames = data.getElementNames();
+			out.close();
+			fOutput.close();
+			System.out.println("Loaded:"+filePath);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	private class Element
 	{
 		public boolean isHovered;
@@ -73,17 +135,40 @@ public class TransitionProbabilityViewer extends Viewer implements JavaAwtRender
 			this.type = type;
 			this.score = score;
 		}
+		@Override
+		public boolean equals(Object obj) {
+			// TODO Auto-generated method stub
+			if(obj instanceof Element)
+			{
+				Element otherElement = (Element) obj;
+				
+				return this.id.equals(otherElement.id) && this.type== otherElement.type;
+				
+			}
+			return super.equals(obj);
+		}
+		@Override
+		public String toString() {
+			return "Element [isHovered=" + isHovered + ", name=" + name
+					+ ", id=" + id + ", type=" + type + ", score=" + score
+					+ "]";
+		}
 	}
 	private void printResult()
 	{
-		for(int i=0;i<countMatrix.length;i++)
+		for(int i=1;i<countMatrix.length;i++)
 		{
-			for(int j=0;j<countMatrix[i].length;j++)
+			for(int j=1;j<countMatrix[i].length;j++)
 			{
-				if(countMatrix[i][j] >0)
+				for(int k=0;k<countMatrix[i][j].length;k++)
 				{
-					String msg= TYPE_NAME[i]+"\t"+TYPE_NAME[j/ TOTAL_OFFSETS]+"\t"+OFFSET_NAME[j%TOTAL_OFFSETS]+"\t"+countMatrix[i][j];
-					System.out.println(msg);
+					if(i != EyeTrackerItem.TYPE_MOVIE_STAR_RATING && j!= EyeTrackerItem.TYPE_MOVIE_STAR_RATING
+							&& countMatrix[i][j][k] > 0 && possibilityMatrix[i][j][k] > 0)
+					{
+						String msg= TYPE_NAME[i]+"\t"+TYPE_NAME[j]+"\t"+CONNECTION_NAME[k]+"\t"+countMatrix[i][j][k]+"\t"+this.possibilityMatrix[i][j][k];
+						System.out.println(msg);
+					}
+					
 				}
 			}
 			
@@ -109,6 +194,11 @@ public class TransitionProbabilityViewer extends Viewer implements JavaAwtRender
 	}
 	private void readTransProbFile(String filePath)
 	{
+		int dotIndex =filePath.lastIndexOf("."); 
+		
+		String taskString = filePath.substring(dotIndex-1, dotIndex);
+		currentTask =Integer.parseInt(taskString);
+		 
 		ArrayList<String> fileTextLines = IOUtil.readTextFile(filePath);
 		ArrayList<Element> lastElementList = new ArrayList<TransitionProbabilityViewer.Element>();
 		ArrayList<Element> currentElementList = new ArrayList<TransitionProbabilityViewer.Element>();
@@ -129,59 +219,57 @@ public class TransitionProbabilityViewer extends Viewer implements JavaAwtRender
 					String[] elementData = element.split(",");
 					if(elementData.length >=COLUMNS_PER_ELEMENT)
 					{
-						if(elementData[0].toUpperCase().equals("HOV") && elementData.length == COLUMNS_PER_ELEMENT)
-						{
-							String name = elementData[1];
-							String id = elementData[2];
-							int type = Integer.parseInt(elementData[3]);
-							Element elem = new Element(true, name, id, type, 0);
-							currentElementList.add(elem);
-						}
-						else if(elementData[0].toUpperCase().equals("HOV") && elementData.length > COLUMNS_PER_ELEMENT)
+						if(elementData[0].toUpperCase().equals(TAG_HOVER) && elementData.length >= COLUMNS_PER_ELEMENT)
 						{
 							int extraLength = elementData.length - COLUMNS_PER_ELEMENT;
 							String name="";
-							for(int i=0;i< extraLength;i++)
+							for(int i=0;i< extraLength+1;i++)
 							{
 								 name+= elementData[1+i]+", ";
 							}
-							name =name.substring(0, name.indexOf(", ")+1);
+							name =name.substring(0, name.indexOf(", "));
 							String id = elementData[2+extraLength];
 							int type = Integer.parseInt(elementData[3+extraLength]);
 							Element elem = new Element(true, name, id, type, 0);
-							currentElementList.add(elem);
+							int elementIndex  = currentElementList.indexOf(elem);
+							if(elementIndex >=0)
+							{
+								currentElementList.get(elementIndex).isHovered = true;
+							}
 						}
-						else if(elementData.length > COLUMNS_PER_ELEMENT)
+						else if(elementData.length >= COLUMNS_PER_ELEMENT)
 						{
 							int extraLength = elementData.length - COLUMNS_PER_ELEMENT;
 							String name="";
-							for(int i=0;i< extraLength;i++)
+							for(int i=0;i< extraLength+1;i++)
 							{
 								 name+= elementData[i]+", ";
 							}
-							name =name.substring(0, name.indexOf(", ")+1);
+							name =name.substring(0, name.indexOf(", "));
 							String id = elementData[1+extraLength];
 							int type = Integer.parseInt(elementData[2+extraLength]);
 							double score = Double.parseDouble(elementData[3+extraLength]);
 							Element elem = new Element(false, name, id, type, score);
 							currentElementList.add(elem);
 						}
-						else
+						
+					}
+					else if(elementData[0].toUpperCase().equals(TAG_VIEW) && elementData.length ==2)
+					{
+						String currentViewString = elementData[1];
+						int elementIndex = this.elementNames.indexOf( new StatElement(currentViewString, currentTask));
+						if(elementIndex>=0)
 						{
-							String name = elementData[0];
-							String id = elementData[1];
-							int type = Integer.parseInt(elementData[2]);
-							double score = Double.parseDouble(elementData[3]);
-							Element elem = new Element(false, name, id, type, score);
-							currentElementList.add(elem);
+							currentView = this.elementNames.get(elementIndex);
 						}
 					}
-					calculate(lastElementList, currentElementList);
-					lastElementList = new ArrayList<TransitionProbabilityViewer.Element>(currentElementList);
-					currentElementList.clear();
-					lastLine = fileLine;
+					
 				}
 				
+				calculate(lastElementList, currentElementList);
+				lastElementList = new ArrayList<TransitionProbabilityViewer.Element>(currentElementList);
+				currentElementList.clear();
+				lastLine = fileLine;
 				
 				//Track Progress
 				index++;
@@ -202,67 +290,135 @@ public class TransitionProbabilityViewer extends Viewer implements JavaAwtRender
 	{
 		if(!lastElementList.isEmpty() && !currentElementList.isEmpty())
 		{
-			boolean isSourceSameType = true;
-			int sourceType=lastElementList.get(0).type;
-			for(Element elem: lastElementList)
-			{
-				isSourceSameType =isSourceSameType && ( elem.type == sourceType);
-			}
 			
-			if(!isSourceSameType)
-			{
-				System.out.println("Not same source type:"+lastElementList);
-			}
-			
-			boolean isSameDestinationType = true;
-			int destinationType=currentElementList.get(0).type;
-			for(Element elem: currentElementList)
-			{
-				isSameDestinationType =isSameDestinationType && ( elem.type == destinationType);
-			}
-			
-			if(!isSameDestinationType)
-			{
-				System.out.println("Not same destination type:"+currentElementList);
-			}
-			if(isSourceSameType && isSameDestinationType)
-			{
-				int offset = OFFSET_NOT_CONNECTED;
-				for(Element source: lastElementList)
+				Element sourceElement = lastElementList.get(0);
+				for(Element item: lastElementList)
 				{
-					int newOffset =  OFFSET_NOT_CONNECTED;
-					for(Element dest: currentElementList)
-					{	
-						boolean isConnected =isConnected(source, dest); 
-						if(isConnected && (source.isHovered || dest.isHovered) && newOffset< OFFSET_CONNECTED_HOVER)
-						{
-							newOffset = OFFSET_CONNECTED_HOVER;
-							break;
-						}
-						else if(isConnected && newOffset< OFFSET_CONNECTED)
-						{
-							newOffset = OFFSET_CONNECTED;
-						}
-					}
-					if( newOffset> offset)
+					if(item.score > sourceElement.score)
 					{
-						offset = newOffset;
-					}
-					
-					if(offset == OFFSET_CONNECTED_HOVER)
-					{
-						break;
+						sourceElement = item;
 					}
 				}
-				countMatrix[sourceType][destinationType * TOTAL_OFFSETS+ offset]++;
-			}
+				
+				Element destinationElement = currentElementList.get(0);
+				
+				for(Element item: currentElementList)
+				{
+					if(item.score > destinationElement.score)
+					{
+						destinationElement = item;
+					}
+				}
+				if(sourceElement.type != EyeTrackerItem.TYPE_MOVIE_STAR_RATING && destinationElement.type != EyeTrackerItem.TYPE_MOVIE_STAR_RATING)
+				{
+					boolean isConnected =isConnected(sourceElement, destinationElement); 
+					int connectionType = CONNECTION_NOT_CONNECTED_NO_HOVER;
+					if(!isConnected && sourceElement.isHovered)
+					{
+						connectionType = CONNECTION_NOT_CONNECTED_SOURCE_HOVER;
+					}
+					else if(!isConnected && destinationElement.isHovered)
+					{
+						connectionType = CONNECTION_NOT_CONNECTED_DESTINATION_HOVER;
+					}
+					else if(
+							isConnected && 
+							!(sourceElement.isHovered || destinationElement.isHovered)
+						)
+					{
+						connectionType = CONNECTION_CONNECTED_NO_HOVER;
+					}
+					else if(isConnected && sourceElement.isHovered)
+					{
+						connectionType = CONNECTION_CONNECTED_SOURCE_HOVER;
+					}
+					else if(isConnected && destinationElement.isHovered)
+					{
+						connectionType = CONNECTION_CONNECTED_DESTINATION_HOVER;
+					}
+					
+					this.countMatrix[sourceElement.type][destinationElement.type][connectionType]++;
+			
+					populatePossibility(sourceElement);
+				}
+				
+				
+				
+				
 			
 		}
 	}
 
+	private void populatePossibility(Element source)
+	{
+		
+		ViewItem sourceItem = getViewItem(source, currentView);	
+
+		
+		if(currentView != null && currentView.getAdjacencyList().containsKey(sourceItem))
+		{
+			HashMap<ViewItem, ArrayList<ViewItem>> adjacencyList = currentView.getAdjacencyList();
+			for( int destinationType = EyeTrackerItem.TYPE_ACTOR;destinationType< TOTAL_TYPES;destinationType++)
+			{
+				ArrayList<ViewItem> allItems = currentView.getItems();			
+				int totalDistinationTypeItems =0;
+				for(ViewItem item: allItems)
+				{
+					if(item.getType() == destinationType)
+					{
+						totalDistinationTypeItems++;
+					}
+				}
+				
+				ArrayList<ViewItem> neighbors = adjacencyList.get(sourceItem);
+				int totalDestinationTypeNeighbors =0;
+				for(ViewItem item: neighbors)
+				{
+					if(item.getType() == destinationType)
+					{
+						totalDestinationTypeNeighbors++;
+					}
+				}
+				int totalNotConnected = totalDistinationTypeItems - totalDestinationTypeNeighbors;
+				//CONNECTION_NOT_CONNECTED_NO_HOVER			
+				this.possibilityMatrix[source.type][destinationType][CONNECTION_CONNECTED_NO_HOVER]+= totalNotConnected;
+				//CONNECTION_NOT_CONNECTED_SOURCE_HOVER
+				this.possibilityMatrix[source.type][destinationType][CONNECTION_NOT_CONNECTED_SOURCE_HOVER]+= totalNotConnected;
+				//CONNECTION_NOT_CONNECTED_DESTINATION_HOVER
+				this.possibilityMatrix[source.type][destinationType][CONNECTION_NOT_CONNECTED_DESTINATION_HOVER]+= totalNotConnected;
+				//CONNECTION_CONNECTED_NO_HOVER
+				this.possibilityMatrix[source.type][destinationType][CONNECTION_CONNECTED_NO_HOVER]+= totalDestinationTypeNeighbors;
+				//CONNECTION_CONNECTED_SOURCE_HOVER
+				this.possibilityMatrix[source.type][destinationType][CONNECTION_CONNECTED_SOURCE_HOVER]+= totalDestinationTypeNeighbors;
+				//CONNECTION_CONNECTED_DESTINATION_HOVER
+				this.possibilityMatrix[source.type][destinationType][CONNECTION_CONNECTED_DESTINATION_HOVER]+= totalDestinationTypeNeighbors;
+			}
+			
+			
+			
+		}
+		
+	}
+	private ViewItem getViewItem(Element elem, StatElement view)
+	{
+		ViewItem item = new ViewItem(Long.parseLong(elem.id), elem.type,elem.name);
+		if(view != null)
+		{
+			int index = view.getItems().indexOf(item);
+			if(index >=0)
+			{
+				item = view.getItems().get(index);
+			}
+		}
+		return item;
+	}
 	private boolean isConnected(Element source, Element dest)
 	{
 		if(source.type == dest.type)
+		{
+			return false;
+		}
+		else if(source.type == EyeTrackerItem.TYPE_MOVIE_STAR_RATING || dest.type == EyeTrackerItem.TYPE_MOVIE_STAR_RATING)
 		{
 			return false;
 		}
@@ -272,52 +428,66 @@ public class TransitionProbabilityViewer extends Viewer implements JavaAwtRender
 		}
 		else if(source.type == EyeTrackerItem.TYPE_MOVIE)
 		{
-			CompactMovie compactMovie = new CompactMovie(Long.parseLong(source.id), source.name, 1900);
-			Movie movie = this.data.getMovie(compactMovie);
-			if(dest.type == EyeTrackerItem.TYPE_ACTOR)
+			if(currentView == null)
 			{
-				CompactPerson actor = new CompactPerson(Long.parseLong(dest.id), dest.name, "m");
-				if(movie.getActors().contains(actor))
+				CompactMovie compactMovie = new CompactMovie(Long.parseLong(source.id), source.name, 1900);
+				Movie movie = this.data.getMovie(compactMovie);
+				if(dest.type == EyeTrackerItem.TYPE_ACTOR)
 				{
-					return true;
+					CompactPerson actor = new CompactPerson(Long.parseLong(dest.id), dest.name, "m");
+					if(movie.getActors().contains(actor))
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+				else if(dest.type == EyeTrackerItem.TYPE_DIRECTOR)
+				{
+					CompactPerson director = new CompactPerson(Long.parseLong(dest.id), dest.name, "m");
+					if(movie.getDirectors().contains(director))
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+				else if(dest.type == EyeTrackerItem.TYPE_GENRE)
+				{
+					Genre genre = new Genre(Long.parseLong(dest.id), dest.name);
+					if(movie.getGenreList().contains(genre))
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+				else if(dest.type == EyeTrackerItem.TYPE_MOVIE_STAR_RATING)
+				{
+					return false;
 				}
 				else
 				{
 					return false;
 				}
-			}
-			else if(dest.type == EyeTrackerItem.TYPE_DIRECTOR)
-			{
-				CompactPerson director = new CompactPerson(Long.parseLong(dest.id), dest.name, "m");
-				if(movie.getDirectors().contains(director))
-				{
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else if(dest.type == EyeTrackerItem.TYPE_GENRE)
-			{
-				Genre genre = new Genre(Long.parseLong(dest.id), dest.name);
-				if(movie.getGenreList().contains(genre))
-				{
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else if(dest.type == EyeTrackerItem.TYPE_MOVIE_STAR_RATING)
-			{
-				return false;
 			}
 			else
 			{
-				return false;
+				
+				ViewItem sourceItem = getViewItem(source, currentView);
+				ViewItem destItem =getViewItem(dest,currentView);
+				
+				return this.currentView.getAdjacencyList().containsKey(sourceItem) 
+						&& this.currentView.getAdjacencyList().get(sourceItem).contains(destItem);
+					
 			}
+			
 		}
 		else if(dest.type == EyeTrackerItem.TYPE_MOVIE)
 		{

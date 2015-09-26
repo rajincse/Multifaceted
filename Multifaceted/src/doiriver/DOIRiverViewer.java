9d335f0f8f444870fe46e5927fa566b31f1e4e0e
rajincse <rajin.cse@gmail.com>
2015-09-26 03:47:43 -0400

    #32
    DOI river implemented with new logic

--- Multifaceted/src/doiriver/DOIRiverViewer.java
+++ Multifaceted/src/doiriver/DOIRiverViewer.java
@@ -2,17 +2,32 @@ package doiriver;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
+
+import multifaceted.FileLineReader;
+import multifaceted.Util;
 
 import perspectives.base.Property;
 import perspectives.base.Viewer;
 import perspectives.properties.PFileInput;
 import perspectives.properties.PFileOutput;
 import perspectives.two_d.JavaAwtRenderer;
+import perspectives.util.SplineFactory;
+import realtime.DataObject;
+import realtime.EyeEvent;
+import scanpath.MultiScanpath;
+import scanpath.ScanpathViewer;
 
 
 public class DOIRiverViewer extends Viewer implements JavaAwtRenderer{
@@ -30,15 +45,23 @@ public class DOIRiverViewer extends Viewer implements JavaAwtRenderer{
 	public static final int INVALID =-1;
 	public static final int INFINITY =10000;
 	
-	public static final int TIME_CELL_WIDTH =20;
-	public static final int TIME_CELL_HEIGHT =10;
-	public static final int CURVE_WIDTH = 5;
-	public static final int DIAGRAM_GAP =10;	
+	public static final int INIT_X =100;
+	public static final int INIT_Y =100;
+	
+	public static final int TIME_CELL_WIDTH =50;
+	public static final int CURVE_WIDTH = 15;	
 	public static final int WIDTH_TITLE = 300;
-	public static final int WIDTH_ANCHOR = 25;
+	public static final int TITLE_X = -300;
 	
 	
-	private DOIRiverDiagram doiRiverDiagram = null;
+	
+	private ArrayList<String> userList = new ArrayList<String>();
+	private ArrayList<ArrayList<EyeEvent>> eyeEventList = new ArrayList<ArrayList<EyeEvent>>();
+	private ArrayList<HashMap<String, DataObject>> dataObjectList = new ArrayList<HashMap<String, DataObject>>();
+	private long startTime =0;
+	private int maxTimeCell =0;
+	
+	private ArrayList<River> riverList = new ArrayList<River>();
 	
 	public DOIRiverViewer(String name) {
 		super(name);
@@ -49,8 +72,7 @@ public class DOIRiverViewer extends Viewer implements JavaAwtRenderer{
 				{
 					@Override
 					protected boolean updating(PFileInput newvalue) {
-											
-						doiRiverDiagram = new DOIRiverDiagram(newvalue.path);
+						process(newvalue.path);
 						requestRender();
 						return super.updating(newvalue);
 					}
@@ -74,6 +96,244 @@ public class DOIRiverViewer extends Viewer implements JavaAwtRenderer{
 		pLoad.setValue(loadFile);
 	}
 
+	private void process(String directoryPath)
+	{
+		File dir = new File(directoryPath);
+		
+		File[] sequenceFiles = dir.listFiles(new FileFilter() {
+			
+			@Override
+			public boolean accept(File file) {
+				// TODO Auto-generated method stub
+				
+				return file.getName().toUpperCase().endsWith(".TXT");
+			}
+		});
+		
+		FileLineReader fileLineReader = new FileLineReader() {
+			
+			@Override
+			public void readLine(String fileLine, File currentFile) {
+				// TODO Auto-generated method stub
+				
+				int index = getUserIndex(currentFile);
+				
+				ArrayList<EyeEvent> eyeEvents = eyeEventList.get(index);
+				HashMap<String, DataObject> dataObjects = dataObjectList.get(index);
+				
+				String[] split = fileLine.split("\t");
+				if (split[0].equals("Eye") && !split[4].trim().equals("5") ){
+	        		
+	        		long t = Long.parseLong(split[1]) ;
+	        		double s = Double.parseDouble(split[5]);
+	        		double p = Double.parseDouble(split[8]);
+	        		
+	        		String objId = split[2];
+	        		DataObject object = new DataObject(objId, split[3], Integer.parseInt(split[4].trim()));
+	        		if(dataObjects.containsKey(objId))
+	        		{
+	        			object = dataObjects.get(objId);
+	        		}
+	        		else
+	        		{
+	        			dataObjects.put(objId, object);
+	        		}
+	        		
+	        		
+	        		
+	        		if(eyeEvents.isEmpty())
+	        		{
+	        			startTime = t;
+	        		}
+	        		EyeEvent e = new EyeEvent(t-startTime,object, s, p);
+	        		eyeEvents.add(e);
+	        	}
+			}
+		};
+		
+		DataObject[][] dataObjectPerTime = new DataObject[sequenceFiles.length][];
+		int index=0;
+		for(File file: sequenceFiles)
+		{
+			String fileName = file.getName();
+			int extensionIndex = fileName.toUpperCase().lastIndexOf(".TXT");
+			String user = fileName.substring(0, extensionIndex);
+			this.userList.add(user);
+			
+			ArrayList<EyeEvent> eyeEvents = new ArrayList<EyeEvent>();
+			this.eyeEventList.add(eyeEvents);
+			
+			HashMap<String, DataObject> dataObjects = new HashMap<String, DataObject>();
+			this.dataObjectList.add(dataObjects);
+			
+			
+			fileLineReader.read(file.getAbsolutePath());
+			
+			dataObjectPerTime[index] = getObjectsPerTime(index);
+			index++;
+			
+		}
+		
+		HashMap<DataObject, HashMap<Integer, Double>> timeScoreMap = new HashMap<DataObject, HashMap<Integer,Double>>();
+		
+		for(int userIndex=0;userIndex<dataObjectPerTime.length;userIndex++)
+		{
+			for(int timeIndex=0;timeIndex<dataObjectPerTime[userIndex].length;timeIndex++)
+			{
+				DataObject object = dataObjectPerTime[userIndex][timeIndex];
+				if(object != null)
+				{
+					if(timeScoreMap.containsKey(object))
+					{
+						HashMap< Integer, Double> timeScore = timeScoreMap.get(object);
+						Integer timeIndexObject = new Integer(timeIndex);
+						if(timeScore.containsKey(timeIndexObject))
+						{
+							Double previousScore = timeScore.get(timeIndexObject);
+							previousScore+= 1.0;
+							timeScore.put(timeIndexObject, previousScore);
+						}
+						else
+						{
+							timeScore.put(timeIndex, 1.0);
+						}
+					}
+					else
+					{
+						HashMap< Integer, Double> timeScore = new HashMap<Integer, Double>();
+						timeScore.put(new Integer(timeIndex), 1.0);
+						timeScoreMap.put(object, timeScore);
+					}	
+				}
+				
+			}
+			if(dataObjectPerTime[userIndex].length > maxTimeCell)
+			{
+				maxTimeCell = dataObjectPerTime[userIndex].length;
+			}
+		}
+		
+		
+		ArrayList<Color> colorList = new ArrayList<Color>();
+		River previousRiver = null;
+		for(DataObject obj :timeScoreMap.keySet())
+		{
+			double[] timeScore = new double[maxTimeCell];
+			for(Integer timeIndex: timeScoreMap.get(obj).keySet())
+			{
+				timeScore[timeIndex] = timeScoreMap.get(obj).get(timeIndex);
+			}
+			Color randomColor = getRandomColor();
+			while(colorList.contains(randomColor))
+			{
+				randomColor = getRandomColor();
+			}
+			River river = new River(obj, timeScore, randomColor);
+			river.setPreviousRiver(previousRiver);
+			previousRiver = river;
+			river.prepareRendering();
+			riverList.add(river);
+		}
+		printDebug();
+	}
+	
+	private Color getRandomColor()
+	{
+		Random rand = new Random();
+		float r = rand.nextFloat() / 2.0f + 0.5f;
+		float g = rand.nextFloat() / 2.0f + 0.5f;
+		float b = rand.nextFloat() / 2.0f + 0.5f;
+		Color randomColor = new Color(r, g, b);
+		
+		return randomColor;
+	}
+	
+	private void printDebug()
+	{
+		String msg="";
+		for(int i=0;i<riverList.size();i++)
+		{
+			River river =riverList.get(i);
+			
+			msg +=river.getDataObject().getLabel()+"-> ";
+			
+			for(int j=0;j<river.getTimeScores().length;j++)
+			{
+				msg+=String.format("%.2f", river.getTimeScores()[j])+", ";
+			}
+			msg+="\r\n";
+		}
+		
+		System.out.println(msg);
+	}
+	
+	public DataObject[] getObjectsPerTime(int userIndex)
+	{
+		ArrayList<EyeEvent> eyeEvents = eyeEventList.get(userIndex);
+		
+		int totalCells =(int)(eyeEvents.get(eyeEvents.size()-1).getTime()/ DOIRiverViewer.TIME_STEP)+1;
+		DataObject[] objects = new DataObject[totalCells];
+		ArrayList<DataObject> dataObjectCollection = new ArrayList<DataObject>();
+		int lastIndex =0;
+		for(EyeEvent eye: eyeEvents)
+		{
+			
+			int index = (int)(eye.getTime()/DOIRiverViewer.TIME_STEP);
+			if(index == lastIndex)
+			{
+				DataObject object = eye.getTarget();
+				if(dataObjectCollection.contains(object))
+				{
+					DataObject savedObject = dataObjectCollection.get(dataObjectCollection.indexOf(object));
+					savedObject.setSortingScore(savedObject.getSortingScore()+eye.getScore());
+				}
+				else
+				{
+					object.setSortingScore(eye.getScore());
+					dataObjectCollection.add(eye.getTarget());
+				}
+				
+			}
+			else
+			{
+				DataObject bestDataObject = getBestDataObject(dataObjectCollection);
+				objects[lastIndex] = bestDataObject;
+				
+				lastIndex = index;
+				dataObjectCollection.clear();
+			}
+		}
+		return objects;
+	}
+	public DataObject getBestDataObject(ArrayList<DataObject> dataObjectCollection)
+	{
+		if(dataObjectCollection != null && !dataObjectCollection.isEmpty())
+		{
+			Collections.sort(dataObjectCollection);
+			
+			return dataObjectCollection.get(0);
+		}
+		else
+		{
+			return null;
+		}
+	}
+	private int getUserIndex(File file)
+	{
+		String fileName = file.getName();
+		int extensionIndex = fileName.toUpperCase().lastIndexOf(".TXT");
+		String user = fileName.substring(0, extensionIndex);
+		
+		if(this.userList != null && !this.userList.isEmpty() && this.userList.contains(user))
+		{
+			return this.userList.indexOf(user);
+		}
+		else
+		{
+			return DOIRiverViewer.INVALID;
+		}
+			
+	}
 	@Override
 	public Color getBackgroundColor() {
 		// TODO Auto-generated method stub
@@ -119,10 +379,37 @@ public class DOIRiverViewer extends Viewer implements JavaAwtRenderer{
 	@Override
 	public void render(Graphics2D g) {
 		// TODO Auto-generated method stub
-		if(doiRiverDiagram != null)
+		g.translate(INIT_X, INIT_Y);
+		
+		
+		g.setColor(Color.black);
+		g.drawLine(0, 0, TIME_CELL_WIDTH*maxTimeCell, 0);
+		
+		for(int i=0;i<riverList.size() ;i++)
 		{
-			doiRiverDiagram.render(g, COLOR_INITIAL_OBJECT, COLOR_INITIAL_LINE_TRANSITION, 1.0);
+			River river = riverList.get(i);
+
+			g.setColor(river.getColor());
+			g.fillPolygon(river.getCurvedPolygonX(), river.getCurvedPolygonY(), river.getCurvedPolygonX().length);
 		}
+		
+		g.translate(TITLE_X, 0);
+		
+		
+		for(int i=0;i<riverList.size();i++)
+		{
+			River river = riverList.get(i);
+			g.setColor(river.getColor());
+			g.fillRect(0,i*CURVE_WIDTH, WIDTH_TITLE-30,CURVE_WIDTH );
+			Util.drawTextBox(g, Color.black, river.getDataObject().getLabel(), new Rectangle(0,i*CURVE_WIDTH, WIDTH_TITLE-30,CURVE_WIDTH ));
+		}
+		
+		
+		g.translate(-TITLE_X, 0);
+		
+		g.translate(-INIT_X, -INIT_Y);
+		
+		
 		
 	}
 	public static double SAVE_VIEW_ZOOM =2;
@@ -131,7 +418,7 @@ public class DOIRiverViewer extends Viewer implements JavaAwtRenderer{
 		
 		// TODO Auto-generated method stub
 		
-		Dimension dimension =doiRiverDiagram.getImageDimension();
+		Dimension dimension =new Dimension(INIT_X-TITLE_X+maxTimeCell*TIME_CELL_WIDTH+300,INIT_Y+riverList.size()*CURVE_WIDTH);
 		
 		
 		if(dimension != null)
@@ -139,8 +426,9 @@ public class DOIRiverViewer extends Viewer implements JavaAwtRenderer{
 			BufferedImage bim = new BufferedImage((int)((dimension.width)* SAVE_VIEW_ZOOM),(int)((dimension.height)*SAVE_VIEW_ZOOM), BufferedImage.TYPE_INT_ARGB);
 			
 			Graphics2D g = bim.createGraphics();
-			
+			g.translate(-INIT_X-TITLE_X+300, -INIT_Y);
 			g.scale(SAVE_VIEW_ZOOM, SAVE_VIEW_ZOOM);
+			
 			render(g);
 			g.dispose();
 			
---
+++ Multifaceted/src/doiriver/River.java
@@ -0,0 +1,232 @@
+package doiriver;
+
+import java.awt.Color;
+import java.awt.Point;
+
+import perspectives.util.SplineFactory;
+
+import realtime.DataObject;
+
+public class River {
+	public DataObject dataObject;
+	
+	private  double[] timeScores;
+	
+	private Color color;
+
+	private River previousRiver =null;
+
+	public River(DataObject dataObject, double[] timeScores, Color color)
+	{
+		this.dataObject = dataObject;
+		this.timeScores = timeScores;
+		this.color = color;
+	}
+	
+	public Point getControlPoint(int timeIndex)
+	{
+		int x =timeIndex * DOIRiverViewer.TIME_CELL_WIDTH+DOIRiverViewer.TIME_CELL_WIDTH/2;
+		int y =0;
+		if(previousRiver != null)
+		{
+			y = previousRiver.getControlPoint(timeIndex).y+(int) this.timeScores[timeIndex] * DOIRiverViewer.CURVE_WIDTH;
+		}
+		else
+		{
+			y = (int) this.timeScores[timeIndex] * DOIRiverViewer.CURVE_WIDTH;
+		}
+		
+		return new Point(x,y);
+	}
+
+	public int[] getOuterEdgePointsX()
+	{
+		int[] xPoints = new int[timeScores.length+2];
+		xPoints[0] =0;
+		for(int i=0;i<timeScores.length;i++)
+		{
+			xPoints[i+1] = getControlPoint(i).x;
+		}
+		
+		xPoints[timeScores.length+1] =timeScores.length * DOIRiverViewer.TIME_CELL_WIDTH;
+		return xPoints;
+	}
+	public int[] getOuterEdgePointsY()
+	{
+		int[] yPoints = new int[timeScores.length+2];
+		yPoints[0] =0;
+		for(int i=0;i<timeScores.length;i++)
+		{
+			yPoints[i+1] = getControlPoint(i).y;
+		}
+		
+		yPoints[timeScores.length+1] =0;
+		return yPoints;
+	}
+	
+	public int[] getPolygonX()
+	{
+		int [] outerEdgeX = this.getOuterEdgePointsX();
+		if(previousRiver != null)
+		{
+			 
+			int[] xPoints = new int[outerEdgeX.length+previousRiver.getOuterEdgePointsX().length-1];
+			int index=0;
+			for(int i=0;i<outerEdgeX.length;i++)
+			{
+				xPoints[i] = outerEdgeX[i];
+				index++;
+			}
+			int[] previousRiverOuterX = previousRiver.getOuterEdgePointsX();
+			for(int i=previousRiverOuterX.length-2;i>0;i--)
+			{
+				xPoints[index] = previousRiverOuterX[i];
+				index++;
+			}
+			
+			return xPoints;
+		}
+		else
+		{
+			int[] xPoints = new int[outerEdgeX.length+1];
+			for(int i=0;i<outerEdgeX.length;i++)
+			{
+				xPoints[i] = outerEdgeX[i];
+			}
+			xPoints[outerEdgeX.length] =0;
+			
+			return xPoints;
+		}
+	}
+	
+	public int[] getPolygonY()
+	{
+		int [] outerEdgeY = this.getOuterEdgePointsY();
+		if(previousRiver != null)
+		{
+			 
+			int[] yPoints = new int[outerEdgeY.length+previousRiver.getOuterEdgePointsY().length-1];
+			int index=0;
+			for(int i=0;i<outerEdgeY.length;i++)
+			{
+				yPoints[i] = outerEdgeY[i];
+				index++;
+			}
+			int[] previousRiverOuterY = previousRiver.getOuterEdgePointsY();
+			for(int i=previousRiverOuterY.length-2;i>0;i--)
+			{
+				yPoints[index] = previousRiverOuterY[i];
+				index++;
+			}
+			
+			return yPoints;
+		}
+		else
+		{
+			int[] yPoints = new int[outerEdgeY.length+1];
+			for(int i=0;i<outerEdgeY.length;i++)
+			{
+				yPoints[i] = outerEdgeY[i];
+			}
+			yPoints[outerEdgeY.length] =0;
+			
+			return yPoints;
+		}
+	}
+	
+	public void prepareRendering()
+	{
+		int[] polygonX = getPolygonX();
+		int[] polygonY = getPolygonY();
+		
+		double[] controlPoints = new double[polygonX.length*3];
+		for(int i=0;i<polygonX.length;i++)
+		{
+			controlPoints[3*i] = polygonX[i];
+			controlPoints[3*i+1] = polygonY[i];
+			controlPoints[3*i+2] = 0;
+		}
+		double[] curvePoints = SplineFactory.createCatmullRom(controlPoints, polygonX.length);
+		this.xPoints  = new int[ curvePoints.length/3];
+		this.yPoints  = new int[ curvePoints.length/3];
+		
+		for(int i=0;i<xPoints.length;i++)
+		{
+			xPoints[i] = (int)curvePoints[3*i];
+			yPoints[i] = (int)curvePoints[3*i+1];
+		}
+	}
+	private int[] xPoints;
+	private int[] yPoints;
+	public int[] getCurvedPolygonX()
+	{
+		return xPoints;
+	}
+	
+	public int[] getCurvedPolygonY()
+	{
+		return yPoints;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public double[] getTimeScores() {
+		return timeScores;
+	}
+
+	public void setTimeScores(double[] timeScores) {
+		this.timeScores = timeScores;
+	}
+
+
+	public DataObject getDataObject() {
+		return dataObject;
+	}
+
+
+	@Override
+	public String toString() {
+		return "River [dataObject=" + dataObject + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((dataObject == null) ? 0 : dataObject.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		River other = (River) obj;
+		if (dataObject == null) {
+			if (other.dataObject != null)
+				return false;
+		} else if (!dataObject.equals(other.dataObject))
+			return false;
+		return true;
+	}
+
+
+	public River getPreviousRiver() {
+		return previousRiver;
+	}
+
+
+	public void setPreviousRiver(River previousRiver) {
+		this.previousRiver = previousRiver;
+	}
+	
+	
+	
+}

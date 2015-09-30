d0ef141e02eef011db2c76851dd1b75353040251
rajincse <rajin.cse@gmail.com>
2015-09-30 04:31:31 -0400

    #34
    AOIEditor changed

--- Multifaceted/src/aoicreator/AOIEditor.java
+++ Multifaceted/src/aoicreator/AOIEditor.java
@@ -2,16 +2,70 @@ package aoicreator;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import perspectives.base.Property;
 import perspectives.base.Viewer;
+import perspectives.properties.PFileInput;
 import perspectives.two_d.JavaAwtRenderer;
 
 public class AOIEditor extends Viewer implements JavaAwtRenderer{
 
+	public static final String PROPERTY_LOAD="Load";
+	
+	private BufferedImage currentImage = null;
+	private String currentImagePath="";
+	private AOIStimuliInfo currentStimuliInfo = null;
 	public AOIEditor(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
+		try
+		{
+			PFileInput fileInput = new PFileInput();
+			fileInput.extensions = new String[]{"png","*"};
+			Property<PFileInput> pLoad = new Property<PFileInput>(PROPERTY_LOAD, fileInput )
+					{
+						@Override
+						protected boolean updating(PFileInput newvalue) {
+							// TODO Auto-generated method stub
+							readImage(newvalue.path);
+							requestRender();
+							return super.updating(newvalue);
+						}
+					};
+		    addProperty(pLoad);
+		}
+		catch(Exception ex)
+		{
+			
+		}
 	}
+	
+	private String getDataFilePath(String imageFilePath)
+	{
+		String extension = imageFilePath.substring(imageFilePath.lastIndexOf("."));
+		String fullPathName = imageFilePath.substring(0, imageFilePath.lastIndexOf(extension));
+		String dataFilePath= fullPathName+".JSON";
+		
+		return dataFilePath;
+	}
+	private void readImage(String imageFilePath)
+	{
+		try {
+			File imageFile = new File(imageFilePath);
+		
+			this.currentImage = ImageIO.read(imageFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 
 	@Override
 	public Color getBackgroundColor() {
@@ -56,9 +110,13 @@ public class AOIEditor extends Viewer implements JavaAwtRenderer{
 	}
 
 	@Override
-	public void render(Graphics2D arg0) {
+	public void render(Graphics2D g) {
 		// TODO Auto-generated method stub
-		
+		if(currentImage != null)
+		{
+			g.drawImage(currentImage, 0, 0, currentImage.getWidth(),  currentImage.getHeight(),
+					0, 0, currentImage.getWidth(), currentImage.getHeight(),null);
+		}
 	}
 
 }

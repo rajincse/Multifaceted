730c7b5dff04adc66eb3a257b9f1ce449533c66c
rajincse <rajin.cse@gmail.com>
2015-12-09 18:49:21 -0500

    #35 MainClass changed

--- Multifaceted/src/multifaceted/main/MainClass.java
+++ Multifaceted/src/multifaceted/main/MainClass.java
@@ -1,5 +1,11 @@
 package multifaceted.main;
 
+import java.text.NumberFormat;
+
+import matrix.TransitionMatrixViewer;
+
+import aoicreator.AOIEditor;
+import doiriver.DOIRiverViewer;
 import eyeinterestanalyzer.EyeInterestAnalyzerViewerFactory;
 import eyeinterestanalyzer.ScarfplotViewer;
 import imdb.IMDBDataFactory;
@@ -8,10 +14,14 @@ import imdb.analysis.HeatMapAnalysisViewerFactory;
 import imdb.analysis.ProbabilityViewerFactory;
 import imdb.analysis.StripViewerFactory;
 import perspectives.base.Environment;
+import perspectives.graph.GraphDataFactory;
+import perspectives.graph.GraphViewerFactory;
 import pivotpath.PivotPathViewerFactory;
 import pivotpath.analysis.PivotPathSimulationViewerFactory;
+import realtime.EyeTrackDataStreamViewer;
 import realtime.StreamDataViewerFactory;
 import recommend.RecommendViewerFactory;
+import scanpath.ScanpathViewer;
 import stat.ElementStatViewerFactory;
 import stat.TransitionProbabilityViewerFactory;
 
@@ -21,17 +31,25 @@ public class MainClass {
 	{
 		Environment e = new Environment(false);
 		e.registerDataSourceFactory(new IMDBDataFactory());
-		e.registerViewerFactory(new IMDBViewerFactory());
+//		e.registerViewerFactory(new IMDBViewerFactory());
 //		e.registerViewerFactory(new StripViewerFactory());
 //		e.registerViewerFactory(new HeatMapAnalysisViewerFactory());
 //		e.registerViewerFactory(new TransitionProbabilityViewerFactory());
 //		e.registerViewerFactory(new RecommendViewerFactory());
-		e.registerViewerFactory(new PivotPathViewerFactory());
+//		e.registerViewerFactory(new PivotPathViewerFactory());
 //		e.registerViewerFactory(new PivotPathSimulationViewerFactory());
-		e.registerViewerFactory(new EyeInterestAnalyzerViewerFactory());
+//		e.registerViewerFactory(new EyeInterestAnalyzerViewerFactory());
 //		e.registerViewerFactory(new ElementStatViewerFactory());
-		e.registerViewerFactory(new StreamDataViewerFactory());
+//		e.registerViewerFactory(new StreamDataViewerFactory());
+//		e.addViewer(new EyeTrackDataStreamViewer("Rajin"));
 //		e.addViewer(new ScarfplotViewer("Scarfplot"));
+//		e.addViewer(new ScanpathViewer("Scanpath"));
+//		e.addViewer(new DOIRiverViewer("DOI River"));
+//		e.addViewer(new EyeInstrument.EyeTester("EyeTester"));
+//		e.addViewer(new AOIEditor("AOI Editor"));
+//		e.registerDataSourceFactory(new GraphDataFactory());
+//		e.registerViewerFactory(new GraphViewerFactory());
+//		e.addViewer(new TransitionMatrixViewer("Matrix"));
 		
 	}
 }
